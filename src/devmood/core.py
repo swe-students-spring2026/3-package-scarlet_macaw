b5f1185b7badcbc006d0443f2d91e7810bbd95ce
blin03 <82618800+blin03@users.noreply.github.com>
2026-03-30 20:39:15 -0400

    implement ascii hug function

--- src/devmood/core.py
+++ src/devmood/core.py
@@ -11,4 +11,17 @@ def passive_aggressive_advice() -> str:
 
 
 def ascii_hug(name: str) -> str:
-    raise NotImplementedError
+    msg = f"Need a hug, {name}?"
+    n = len(msg)
+
+    top    = "  _" + "_" * n + "_"
+    middle = " / " + msg + " \\"
+    bottom = " \\_" + "_" * n + "_/"
+    tail   = "    \\"
+    
+    bunny1 = "      (\\_/)"
+    bunny2 = "     (>'.'<)"
+    bunny3 = '     (")_(")'
+
+    return "\n".join([top, middle, bottom, tail, bunny1, bunny2, bunny3])
+    
